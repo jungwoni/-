dc38d0af578b9d368527c4679fe987e1f4dcee6e
LG <LG@DESKTOP-SRUM8SC>
2018-12-14 22:07:51 +0900

    2개의 값을 입력받아 사칙연산 계산

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,6 +1,19 @@
+import java.util.Scanner;
 
 public class Calculator {
 	public static void main(String[] args) {
-		System.out.println("µ¡¼À: " + (3 + 4));
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ã¹ ¹øÂ° ÀÔ·Â °ª: ");
+		int first = scanner.nextInt();
+		System.out.println(first);
+		
+		System.out.println("Ã¹ ¹øÂ° ÀÔ·Â °ª: ");
+		int second = scanner.nextInt();
+		System.out.println(second);
+		
+		System.out.println("µ¡¼À: " + (first + second));;
+		System.out.println("»¬¼À: " + (first - second));;
+		System.out.println("°ö¼À: " + (first * second));;
+		System.out.println("³ª´°¼À: " + (first / second));;
 	}
 }
