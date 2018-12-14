e034e25035a27e60adec5925db0ae571c49756de
LG <LG@DESKTOP-SRUM8SC>
2018-12-14 22:22:27 +0900

    사칙연산 기호에 따른 계산을 함

--- src/Calculator.java
+++ src/Calculator.java
@@ -7,13 +7,29 @@ public class Calculator {
 		int first = scanner.nextInt();
 		System.out.println(first);
 		
-		System.out.println("Ã¹ ¹øÂ° ÀÔ·Â °ª: ");
+		System.out.println("»çÄ¢¿¬»ê ±âÈ£: ");
+		String symbol = scanner.next();
+		System.out.println(symbol);
+		
+		System.out.println("µÎ ¹øÂ° ÀÔ·Â °ª: ");
 		int second = scanner.nextInt();
 		System.out.println(second);
 		
-		System.out.println("µ¡¼À: " + (first + second));;
-		System.out.println("»¬¼À: " + (first - second));;
-		System.out.println("°ö¼À: " + (first * second));;
-		System.out.println("³ª´°¼À: " + (first / second));;
+		if(symbol.equals("+")) {
+			System.out.println("µ¡¼À: " + (first + second));
+		} else if(symbol.equals("-")) {
+			System.out.println("»¬¼À: " + (first - second));
+		} else if(symbol.equals("*")) {
+			System.out.println("°ö¼À: " + (first * second));
+		} else if(symbol.equals("-*")) {
+			System.out.println("³ª´°¼À: " + (first / second));
+		} else {
+			System.out.println("»çÄ¢¿¬»ê ±âÈ£°¡ ¾Æ´Õ´Ù.");
+		}
+		
+		System.out.println("µ¡¼À: " + (first + second));
+		System.out.println("»¬¼À: " + (first - second));
+		System.out.println("°ö¼À: " + (first * second));
+		System.out.println("³ª´°¼À: " + (first / second));
 	}
 }
